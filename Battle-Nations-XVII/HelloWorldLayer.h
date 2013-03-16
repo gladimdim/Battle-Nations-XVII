c54c737c20494c857e2cd397b72e0a4019b53643
Dmytro Gladkyi <gladimdim@gmail.com>
2013-03-16 14:16:49 +0200

    Initial Commit

---
+++ Battle-Nations-XVII/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Battle-Nations-XVII
+//
+//  Created by Dmytro Gladkyi on 3/16/13.
+//  Copyright Dmytro Gladkyi 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
